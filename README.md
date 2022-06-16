06b7b241e87ed236ec2e472ef5888ac1129ae56c
Yogesh K <armsvcmode@gmail.com>
2022-06-16 20:25:59 +0530

    Create the /utils page to add fake subreddits, fake users, and fake content

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Requirements
+
+- creating the model for holding the application data
+- we’ll create a bunch of fake communities, fake users, and inside them fake content
+- we’ll show the content in the homepage
+- letting people signup/login to the website and pick a username
+- after login, the user will be able to interact with content by upvoting or downvoting posts
+- a user can add posts to a subreddit
+- a user can add images to a post
+- a user can add comments to posts
+- comments can be nested (you can comment another comment)
+- a user has a profile
+- isolated communities (subreddits)
+- nested comments (we had comments in the Twitter clone, but you could not reply to replies to a Tweet, so no nested comments)
+- a upvote/downvote mechanism
+- uploading images to AWS S3
---
+++ pages/api/utils.js
@@ -0,0 +1,116 @@
+/* Handle API route for /utils */
+
+import prisma from 'lib/prisma'
+import { faker } from '@faker-js/faker'
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') return res.end()
+
+    // generate 10 fake users
+    if (req.body.task === 'generate_users') {
+        let count = 0
+        while (count < 10) {
+            await prisma.user.create({
+            data: {
+                name: faker.internet.userName().toLowerCase(),
+                email: faker.internet.email().toLowerCase(),
+            },
+            })
+            count++
+        }
+    }
+
+    if (req.body.task === 'generate_subreddits') {
+        let count = 0
+        while (count < 10) {
+            await prisma.subreddit.create({
+            data: {
+                name: faker.word.noun().toLowerCase(),
+                description: faker.lorem.paragraph(1).toLowerCase(),
+            },
+            })
+        count++
+     }
+    }
+
+    // First we find the list of subreddits, and for each subreddit we’re going 
+    // to add a post:
+    if (req.body.task === 'add_fake_content') {
+
+        const users = await prisma.user.findMany()
+
+        const getRandomUser = () => {
+            const randomIndex = Math.floor(Math.random() * users.length)
+            return users[randomIndex]
+        }
+
+        const createPostFromRandomUser = async (subreddit) => {
+            const user = getRandomUser()
+            // Once we have the random user, we can create a post with some
+            // fake content and we connect it to the user and to the subreddit
+            return await prisma.post.create({
+                data: {
+                    title:
+                        faker.hacker.adjective() +
+                        ' ' +
+                        faker.hacker.verb() +
+                        ' ' +
+                        faker.hacker.noun(),
+                    content: faker.hacker.phrase(),
+                    subreddit: {
+                        connect: {
+                            name: subreddit.name,
+                        },
+                    },
+                    author: {
+                        connect: { id: user.id },
+                    },
+                },
+                })
+        }
+
+        const createCommentsToPost = async (post) => {
+            let count = 0
+            const commentsNumber = Math.floor(Math.random() * 5)
+
+            while (count < commentsNumber) {
+                const user = getRandomUser()
+
+                await prisma.comment.create({
+                    data: {
+                    content: faker.hacker.phrase(),
+                    post: {
+                        connect: { id: post.id },
+                    },
+                    author: {
+                        connect: { id: user.id },
+                    },
+                 },
+                })
+                count++
+            }
+        }
+        // Step1 Finding list of subreddits
+        const subreddits = await prisma.subreddit.findMany()
+        subreddits.forEach(async (subreddit) => {
+            // Step2 add post for each subreddit (first get the post and then the 
+            // comments for the post)
+            const post = await createPostFromRandomUser(subreddit)
+
+            // Step3 : Add comment to each post
+            await createCommentsToPost(post)
+        })
+    }
+
+
+
+    // deletes all comments, posts, subreddits, users... everything.
+    if (req.body.task === 'clean_database') {
+        await prisma.comment.deleteMany({})
+        await prisma.post.deleteMany({})
+        await prisma.subreddit.deleteMany({})
+        await prisma.user.deleteMany({})
+    }
+
+  res.end()
+}
