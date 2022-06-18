b8be6af6c66f7292e966c3c7496efb09fdf641b8
Yogesh K <armsvcmode@gmail.com>
2022-06-18 14:12:41 +0530

    Use a consistent header

--- pages/r/[subreddit].js
+++ pages/r/[subreddit].js
@@ -13,13 +13,17 @@ export default function Subreddit({ subreddit, posts }) {
     return (
         <>
          {/*link to the homepage from a subreddit page*/}
-        <Link href={`/`}>
-            <a className='text-center p-5 underline block'>
-            🔙 back to the homepage
-            </a>
-        </Link>
-        <p className='text-center p-5'>/r/{subreddit.name}</p>
-         <Posts posts={posts} />
+            <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+                <Link href={`/`}>
+                    <a className='underline'>Home</a>
+                </Link>
+                <p className='grow'></p>
+            </header>
+            <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+                <p className='text-center'>/r/{subreddit.name}</p>
+                <p className='ml-4 text-left grow'>{subreddit.description}</p>
+            </header>
+            <Posts posts={posts} />
         </>
     )
 }
