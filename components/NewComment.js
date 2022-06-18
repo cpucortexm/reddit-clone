894fec62d0c993b9d5ee4abf24bd38aa430f1bce
Yogesh K <armsvcmode@gmail.com>
2022-06-18 16:45:50 +0530

    Allow  people to add comments for a post.

---
+++ components/NewComment.js
@@ -0,0 +1,51 @@
+// Comment can be added in a post
+// Uses the API route /api/comment
+
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+export default function NewComment({ post }) {
+  const router = useRouter()
+  const [content, setContent] = useState('')
+
+  const onclickSubmit = async(e) =>{
+    e.preventDefault()
+    if (!content) {
+        alert('Enter some text in the comment')
+          return
+    }
+    const requestParams = {
+        body: JSON.stringify({
+                    post: post.id,
+                    content: content,
+                }),
+        headers: {
+                'Content-Type': 'application/json',
+                },
+        method: 'POST',
+    }
+    const res = await fetch('/api/comment',requestParams)
+    // reload this page
+    router.reload(window.location.pathname)
+   }
+
+  return (
+    <form
+      className='flex flex-col mt-10'
+      onSubmit={onclickSubmit}
+    >
+      <textarea
+        className='border border-gray-700 p-4 w-full text-lg font-medium bg-transparent outline-none color-primary '
+        rows={1}
+        cols={50}
+        placeholder='Add a comment'
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <div className='mt-5'>
+        <button className='border border-gray-700 px-8 py-2 mt-0 mr-8 font-bold '>
+          Comment
+        </button>
+      </div>
+    </form>
+  )
+}
